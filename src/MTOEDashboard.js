1502869b58a740daf43fd6d9726e259f7ec3f80a
Richard-codm <grace4flying@gmail.com>
2025-07-12 18:26:01 +0100

    Create src/MTOEDashboard.js

---
+++ src/MTOEDashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MTOEDashboard() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>🧪 MTOE Dashboard</h1>
+      <p>Entropy Clock and tools coming soon...</p>
+    </div>
+  );
+}
