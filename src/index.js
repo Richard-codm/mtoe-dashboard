b515705ecfee0314c6323624737cc5aead94b241
Richard-codm <grace4flying@gmail.com>
2025-07-12 18:25:06 +0100

    Create src/index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MTOEDashboard from './MTOEDashboard';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MTOEDashboard />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
